d4f91564bff60a0c153b19ef2a3cc1b6170d04af
Aral Tasher <novel@novel.local>
2018-10-12 10:32:02 -0400

    intor to styled componenets

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+background: red;
+font-size: 50px;
+span {
+    font-size: 100px;
+}
+`;
 
 class Page extends Component {
     render() {
@@ -8,6 +17,7 @@ class Page extends Component {
             <div>
                 <Meta />
                 <Header />
+                <MyButton>Click <span>Me</span></MyButton>
                 <p>Hey I'm the page component</p>
                 {this.props.children}
             </div>
