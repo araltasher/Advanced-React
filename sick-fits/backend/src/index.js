e5e4760715334dae8b6669438b7dbeb00845f982
Aral Tasher <novel@novel.local>
2019-01-02 14:14:06 -0500

    handle user info in cookies

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -8,7 +9,16 @@ const server = createServer();
 //  Use express middleware to handle cookies
 server.express.use(cookieParser());
 
-//  TODO: User express middleware to populate current user
+//  Decode the JWT to get the userID
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    //  put userID on req for future access requests
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
