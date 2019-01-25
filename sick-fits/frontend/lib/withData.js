0a23480cb83d962c4259f0cf1171c4828d667888
Aral Tasher <novel@novel.local>
2019-01-25 13:17:57 -0500

    front end cart completed

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -1,5 +1,6 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
+import { LOCAL_STATE_QUERY } from '../components/Cart';
 import { endpoint } from '../config';
 
 function createClient({ headers }) {
@@ -12,6 +13,27 @@ function createClient({ headers }) {
         },
         headers,
       });
+    },
+    clientState: {
+      resolvers: {
+        Mutation: {
+          toggleCart(_, variables, { cache }) {
+            //  Read the cartOpen value from the cache
+            const { cartOpen } = cache.readQuery({
+              query: LOCAL_STATE_QUERY,
+            });
+            //  Toggle cart state
+            const data = {
+              data: { cartOpen: !cartOpen },
+            };
+            cache.writeData(data);
+            return data;
+          },
+        },
+      },
+      defaults: {
+        cartOpen: false,
+      },
     },
   });
 }
